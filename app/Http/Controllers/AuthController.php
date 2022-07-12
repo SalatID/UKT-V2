4393fd11ef73ce97f62664cd6f2d769acc3a4fd1
Mursalat Asyidiq <mursalatasyidiq@gmail.com>
2022-07-12 09:59:22 +0700

    validasi event berakhir

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\EventMaster;
+use DB;
 
 class AuthController extends Controller
 {
@@ -15,18 +17,22 @@ class AuthController extends Controller
     {
       $credentials = request()->only('email', 'password');
       if (auth()->attempt($credentials)) {
-         
-          $sessionValue = [
-            "userData"=>[
-              "id"=>auth()->user()->id,
-              "name"=>auth()->user()->name,
-              "email"=>auth()->user()->email,
-              "role"=>auth()->user()->role,
-              "komwil_id"=>auth()->user()->komwil_id,
-              "event_id"=>auth()->user()->event_id
-            ]
-          ];
-          request()->session()->put("userData",$sessionValue);
+         if(auth()->user()->role!='SPADM'){
+           if(!EventMaster::where('id',auth()->user()->event_id)->where([ ['tgl_mulai','<=',date('Y-m-d')], [DB::raw('DATE_ADD(tgl_selesai, INTERVAL 1 MONTH)'),'>=',date('Y-m-d')]])->exists()){
+            return redirect()->route('login')->with(["error"=>true,"message"=>"Event Anda Sudah Berakhir, harap hubungi administrator."]);
+           }
+         }
+          // $sessionValue = [
+          //   "userData"=>[
+          //     "id"=>auth()->user()->id,
+          //     "name"=>auth()->user()->name,
+          //     "email"=>auth()->user()->email,
+          //     "role"=>auth()->user()->role,
+          //     "komwil_id"=>auth()->user()->komwil_id,
+          //     "event_id"=>auth()->user()->event_id
+          //   ]
+          // ];
+          // request()->session()->put("userData",$sessionValue);
           return redirect()->route('home');;
       }
       return redirect()->back()->with(["error"=>true,"message"=>"Email atau Password salah"]);
