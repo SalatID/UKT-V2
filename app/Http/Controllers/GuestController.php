6a08275adc41694f03c6f12c95de7a0c1f72a3a8
Mursalat Asyidiq <mursalatasyidiq@gmail.com>
2022-07-13 21:15:53 +0700

    Update GuestController.php

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -121,7 +121,8 @@ class GuestController extends Controller
     {
         try {
             $decrypted = Crypt::decrypt($no_peserta);
-            $peserta = Peserta::where('no_peserta','002')->first();
+            $pesertaa = Peserta::where('no_peserta','002')->first();
+            dd($pesertaa);
             $event = EventMaster::where('id',$peserta->event_id)->first();
             $qrCode = QrCode::size(100)->generate(route('self-peserta',[$event->event_alias]));
             return view('guest.peserta',compact('event','peserta','qrCode'));
