dfdc40ada39469bf3c32d98b15fec704c7f84f1b
SalatID <31203124+SalatID@users.noreply.github.com>
2022-12-04 19:48:29 +0700

    Update penilaian.blade.php

--- resources/views/guest/penilaian.blade.php
+++ resources/views/guest/penilaian.blade.php
@@ -5,7 +5,7 @@
         <tr>
             <td colspan="2">
                 <br>
-                <a href="#" onclick="backToHome()">Kembali Ke Halaman Awal</a>
+                <a href="{{env('APP_URL')}}/event/run/{{$alias}}">Kembali Ke Halaman Awal</a>
                 <br>
                 <br>
                 <br>
@@ -78,9 +78,4 @@
             </tr>
         </table>
     </form>
-    <script>
-        function backToHome(){
-            location.reload()
-        }
-    </script>
 @endsection
