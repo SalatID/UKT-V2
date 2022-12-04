d148fb29f0f1c736d1e3aa3981a10c7b981ba6dd
SalatID <31203124+SalatID@users.noreply.github.com>
2022-12-04 19:56:38 +0700

    Update sidebar.blade.php

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -15,7 +15,7 @@
         </div>
         <div class="info">
           <a href="#" class="d-block">{{auth()->user()->name}}</a>
-          <span class="text-white">{{\App\Models\EventMaster::where('id',auth()->user()->event_id)->first()->name??'No Event'}} {{\App\Models\EventMaster::where('id',auth()->user()->event_id)->first()->penyelenggara??'No Event'}}</span>
+          <p class="text-white text-wrap col-12">{{\App\Models\EventMaster::where('id',auth()->user()->event_id)->first()->name??'No Event'}} {{\App\Models\EventMaster::where('id',auth()->user()->event_id)->first()->penyelenggara??'No Event'}}</p>
         </div>
       </div>
 
