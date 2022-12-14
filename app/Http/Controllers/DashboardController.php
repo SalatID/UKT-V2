3bdcfde56c74a3251fbdee80cb181a9f425dadb3
SalatID <31203124+SalatID@users.noreply.github.com>
2022-12-14 14:36:50 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,15 +17,15 @@ class DashboardController extends Controller
         if(request()->has('event_alias') || auth()->user()->event_id!=null){
             if(request()->has('event_alias')) $event = EventMaster::where('event_alias',request('event_alias'))->first();
             if(auth()->user()->event_id!=null) $event = EventMaster::where('id',auth()->user()->event_id)->first();
-            $sumKomwil = Peserta::selectRaw('count(*) as y,b.name')
+            $sumKomwil = Peserta::selectRaw('cast(count(*) as int)as y,b.name')
                         ->join('komwil as b','b.id','peserta.komwil_id')
                         ->where('event_id',$event->id)
                         ->groupBy('b.name')->get(); 
-            $sumTs =  Peserta::selectRaw('count(*) as y,b.name')
+            $sumTs =  Peserta::selectRaw('cast(count(*) as int) as y,b.name')
                         ->join('ts as b','b.id','peserta.ts_awal_id')
                         ->where('event_id',$event->id)
                         ->groupBy('b.name')->orderBy('b.id')->get(); 
-            $sumJenjang =  Peserta::selectRaw('count(*) as y,tingkat as name')
+            $sumJenjang =  Peserta::selectRaw('cast(count(*) as int) as y,tingkat as name')
                         ->where('event_id',$event->id)
                         ->groupBy('tingkat')->get(); 
         }
