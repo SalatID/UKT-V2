84121b22cb1cb22e467e527693a674b0f82cbc96
SalatID <31203124+SalatID@users.noreply.github.com>
2022-12-18 15:58:02 +0700

    Update index.blade.php

--- resources/views/admin/penilai/index.blade.php
+++ resources/views/admin/penilai/index.blade.php
@@ -11,7 +11,7 @@
                     <tr>
                         <th>#</th>
                         <th>Nama Penilai</th>
-                        <th>TS</th>
+                        {{-- <th>TS</th> --}}
                         <th>Komwil</th>
                         <th>Event</th>
                         <th>Action</th>
@@ -23,7 +23,7 @@
                         <tr>
                             <td>{{ $no++ }}</td>
                             <td>{{ $item->name }}</td>
-                            <td>{{ $item->data_ts->name }}</td>
+                            {{-- <td>{{ $item->data_ts->name }}</td> --}}
                             <td>{{ $item->data_komwil->name??'' }}</td>
                             <td>{{ $item->data_event->name??'' }}</td>
                             <td>
