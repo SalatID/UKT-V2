c06e29a6d1a94e879b13f71ece2259c8b9bf33ed
SalatID <31203124+SalatID@users.noreply.github.com>
2022-12-28 10:47:25 +0700

    Update index.blade.php

--- resources/views/admin/nilai/index.blade.php
+++ resources/views/admin/nilai/index.blade.php
@@ -106,7 +106,7 @@
                     @foreach ($dataNilai as $item)
                         <tr>
                             <td>{{ $no++ }}</td>
-                            <td>{{ $item->data_peserta->name?? }}</td>
+                            <td>{{ $item->data_peserta->name??'' }}</td>
                             <td>{{ $item->data_peserta->data_ts->name??'' }}</td>
                             <td>{{ $item->data_peserta->data_unit->name??'' }}</td>
                             <td>{{ $item->data_peserta->data_komwil->name??'' }}</td>
