080ef879105b8195c341249e28f18a9ef9569498
Mursalat Asyidiq <mursalat.asyidiq@spesolution.com>
2023-12-24 22:19:19 +0700

    Update belakang.blade.php

--- resources/views/admin/sertifikat/belakang/belakang.blade.php
+++ resources/views/admin/sertifikat/belakang/belakang.blade.php
@@ -198,7 +198,7 @@
                                 Ketua PPS Satria Muda Indonesia<br>
                                 Komwil Jakarta Barat
                                 <br>
-                                <img src="{{public_path()}}/ttd/ttd-ka-indra.png" width="150px" alt="" >
+                                <img src="{{public_path()}}/ttd/ttd-ka-indra.png" width="150px" style="margin-bottom: -30px;" >
                                 <br>
                             </td>
                         </tr>
