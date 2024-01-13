242b88860028d5bdf819e428e1b03a3473df4fb2
Mursalat Asyidiq <mursalat.asyidiq@spesolution.com>
2024-01-13 22:16:30 +0700

    Update SummaryNilaiDetail.php

--- app/Models/SummaryNilaiDetail.php
+++ app/Models/SummaryNilaiDetail.php
@@ -24,13 +24,13 @@ class SummaryNilaiDetail extends Model
     public function criteria($nilai)
     {
         Log::info($nilai);
-        if($nilai <4 ){
+        if($nilai <=4 ){
             return 'KURANG';
         }
-        if($nilai >4 && $nilai <6 ){
+        if($nilai >4 && $nilai <=6 ){
             return 'CUKUP';
         }
-       if($nilai >=6 && $nilai <=8){
+       if($nilai >6 && $nilai <=8){
            return 'BAIK';
        }
        return 'AMAT BAIK';
