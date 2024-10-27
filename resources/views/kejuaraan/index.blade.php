ac6210d984f769552e12a2a919c3f7655aa1f53d
Mursalat Asyidiq <mursalat.asyidiq@spesolution.com>
2024-10-27 11:44:01 +0700

    fix

--- resources/views/kejuaraan/index.blade.php
+++ resources/views/kejuaraan/index.blade.php
@@ -105,7 +105,7 @@
         $('#kategoriUsia').val(data.kategori_usia);
         $('#kelasSekolah').val(data.kelas_sekolah);
         $('#beratBadan').val(data.berat_badan);
-        $('#kategoriPertandingan').val(data.weight.id??'');
+        $('#kategoriPertandingan').val(data.weight= null? data.weight.id:'');
         $('#usia').val(data.usia);
         $('#editPeserta').modal('show')
     }
