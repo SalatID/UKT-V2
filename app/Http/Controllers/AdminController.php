4cc100db74a11aab339913a389dfad37b50f22c0
Mursalat Asyidiq <mursalat.asyidiq@spesolution.com>
2024-12-21 14:09:14 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -117,7 +117,7 @@ class AdminController extends Controller
             WHERE 
                 ts.id NOT IN (1, 11)
             ORDER BY 
-                ts.id, ranked.rank, ranked.tingkat,ranked.nilai DESC;
+                ts.id, ranked.tingkat,ranked.rank, ranked.nilai DESC;
 
             ",[
                 'event_id'=>$event->id,
